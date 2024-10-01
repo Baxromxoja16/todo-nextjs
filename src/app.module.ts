3b0f5ed8fe1d1f6cf3a28bad36eb3b2b2e94ffbd
baxromxoja16 <aglamof@mail.ru>
2024-10-01 10:59:25 +0500

    feat: added todo files connected to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodoModule } from './todos/todo.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as dotenv from 'dotenv';
+
+dotenv.config(); // Load environment variables
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(process.env.MONGO_URI), TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
